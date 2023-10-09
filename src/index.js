9670ffbf7a19ea5337a649fc8f938f3fa06d6173
HNE-K <eunicekiang@ymail.com>
2023-10-08 20:29:43 -0400

    world expanded horizontally

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // must define variables before loading page
 var dragobj = null;
 var art1 = null;
+var art2 = null;
+var art3 = null;
 var slider = null;
 var oLeft = 0;
 var oTop = 0;
@@ -29,32 +31,49 @@ function updatePos(Ev) {
     if (Ev.keyCode) iKeyCode = Ev.keyCode;
     else if (Ev.which) iKeyCode = Ev.which;
     switch (iKeyCode) {
-        case 37:
+        case 65: // 37 is Left, 65 is A
             oLeft -= speed;
             break;
-        case 38:
+        case 87: // 38 is Up, 87 is W
             oTop -= speed;
             break;
-        case 39:
+        case 68: // 39 is Right, 58 is D
             oLeft += speed;
             break;
-        case 40:
+        case 83: // 40 is Down, 83 is S
             oTop += speed;
             break;
         default:
             break;
     }
-//     console.log(oLeft, oTop);
-//     console.log(art1.offsetLeft, art1.offsetTop);
+
     if (Math.abs(oLeft - art1.offsetLeft) <= 50 && Math.abs(oTop - art1.offsetTop) <= 50) {
         console.log("reached range");
-        window.location.href = "art1.html";
+        document.getElementById("art1Preview").style.visibility = "visible";
+    } else {
+        document.getElementById("art1Preview").style.visibility = "hidden";
+    }
+    
+    if (Math.abs(oLeft - art2.offsetLeft) <= 50 && Math.abs(oTop - art2.offsetTop) <= 50) {
+        console.log("reached range");
+        document.getElementById("art2Preview").style.visibility = "visible";
+    } else {
+        document.getElementById("art2Preview").style.visibility = "hidden";
+    }
+
+    if (Math.abs(oLeft - art3.offsetLeft) <= 50 && Math.abs(oTop - art3.offsetTop) <= 50) {
+        console.log("reached range");
+        document.getElementById("art3Preview").style.visibility = "visible";
+    } else {
+        document.getElementById("art3Preview").style.visibility = "hidden";
     }
 }
 
 function init() {
     dragobj = document.getElementById("obj1");
     art1 = document.getElementById("art1");
+    art2 = document.getElementById("art2");
+    art3 = document.getElementById("art3");
     document.onkeydown = startMove;
     document.onkeypress = moving;
     document.onkeydown = moving;
